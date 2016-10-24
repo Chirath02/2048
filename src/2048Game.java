739bcda2e54057565e768d2202fa670867192bf6
Chirath02 <chirath.02@gmail.com>
2016-10-24 13:26:20 +0530

    Initial

--- src/2048Game.java
+++ src/2048Game.java
@@ -1,10 +1,45 @@
-package bin;
-
 import java.io.*;
 import java.util.*;
 
 class Game2048 {
-    public static void main(String[] args) {
-	System.out.println("Hello World");
+  int gameSize = 4;
+  int initialNumberofTiles = 2, initialTileValue = 2;
+  int tiles[][] = new int[gameSize][gameSize];
+  int max = gameSize-1, min = 0;
+  Random rand = new Random();
+  Scanner scanner = new Scanner(Syatem.in);
+
+  public void initialize(int number) {
+    for(int i =0; i < number;) {
+      int x = rand.nextInt((max - min) + 1) + min;
+      int y = rand.nextInt((max - min) + 1) + min;
+      if(tiles[x][y] == 0) {
+        i++;
+        tiles[x][y] = initialTileValue;
+      }
     }
+  }
+
+  public void printTiles() {
+    System.out.println();
+    for(int i = 0; i < gameSize; ++i) {
+      System.out.print("| ");
+      for(int j = 0; j < gameSize; ++j) {
+        System.out.print(tiles[i][j] + "| ");
+      }
+      System.out.println();
+    }
+  }
+
+  public startGame() {
+
+  }
+
+
+  public static void main(String[] args) {
+    Game2048 game = new Game2048();
+    game.initialize(initialTileValue);
+
+    game.printTiles();
+  }
 }
